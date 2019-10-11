d68dd2e8916669ea6c6e1261e3436f6e649dd59f
Olaf Łukaszewicz <129854@student.uwm.edu.pl>
2019-10-11 21:21:46 +0200

    -Added DeleteProduct command

---
+++ SoftwareHutPreview.Application/Product/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace SoftwareHutPreview.Application.Product.Commands.DeleteProduct
+{
+    public class DeleteProductCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
