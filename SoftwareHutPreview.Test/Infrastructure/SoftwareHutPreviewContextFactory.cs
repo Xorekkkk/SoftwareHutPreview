101d8f600b8932a5abc135633ff8084d1c713eec
Olaf Łukaszewicz <129854@student.uwm.edu.pl>
2019-10-15 12:43:22 +0200

    -Added test project to solution -Creater first test "CreateProductShouldReturnId" -Added FluentAssertions package

---
+++ SoftwareHutPreview.Test/Infrastructure/SoftwareHutPreviewContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftwareHutPreview.Domain.Entities;
+using SoftwareHutPreview.Persistence;
+
+namespace SoftwareHutPreview.Test.Infrastructure
+{
+    public class SoftwareHutPreviewContextFactory
+    {
+        public static SoftwareHutPreviewDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<SoftwareHutPreviewDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new SoftwareHutPreviewDbContext(options);
+
+            context.Database.EnsureCreated();
+
+            context.SaveChanges();
+
+            return context;
+        }
+
+        public static void Destroy(SoftwareHutPreviewDbContext context)
+        {
+            context.Database.EnsureDeleted();
+
+            context.Dispose();
+        }
+
+    }
+}
---
+++ SoftwareHutPreview.Test/Products/Commands/CreateProductCommandTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using FluentAssertions;
+using Mapster;
+using SoftwareHutPreview.Application.Product.Commands.CreateProduct;
+using SoftwareHutPreview.Application.Product.ViewModels;
+using SoftwareHutPreview.Test.Infrastructure;
+using Xunit;
+
+namespace SoftwareHutPreview.Test.Products.Commands
+{
+    public class CreateProductCommandTest : CommandTestBase
+    {
+        [Fact]
+        public async void CreatedProductShouldReturnId()
+        {
+            var sut = new CreateProductHandler(_context);
+
+            var response = await sut
+                .Handle(
+                    new CreateProductCommand
+                    {
+                        Name = "Intel i9-9900k",
+                        Description = "Nowy niezawodny",
+                        Category = 1.Adapt<CategoryViewModel>()
+                    }, CancellationToken.None);
+
+            response.Should().BeOfType(typeof(int));
+
+        }
+    }
+}
